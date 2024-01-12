e2edca312c4e96b1e9fb2a9619b081411c816bf2
root <root@JuliaPC>
2024-01-12 20:16:40 -0300

    Clear screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -38,54 +38,6 @@ function Home() {
    return (
       <>
          <Wrapper>
-            <Menu />
-
-            <ContainerHome>
-               <SideRope />
-               <ContentText>
-                  <IntroText>
-                     Hi! Welcome, I'm Juliana
-                  </IntroText>
-                  <Subtitle>
-                     UX/UI Design & web developer
-                  </Subtitle>
-               </ContentText>
-               <PencilCupImage src={PencilCup} alt="Cup with pencil" />
-               <ContentSocialMedia>
-                  <SocialMedia />
-               </ContentSocialMedia>
-            </ContainerHome>
-
-            <Divider />
-
-            <ContainerAbout>
-               <ContentImage>
-                  <BookshelfImage src={Bookshelf} alt="Cup with pencil" />
-               </ContentImage>
-               
-               <ContentTextAbout>
-                  <SimpleTitle text="About me" color="#EFE0E8" />
-                  <p>
-                  I like to explore new technologies and develop solutions. 
-                  I am currently studying Cross-platform software development. 
-                  I'm on my way to learning more about front-end technologies. 
-                  In my free time, I make new projects, create arts and templates. 
-                  Feel free to contact me for consultancy and pro bono volunteering, 
-                  or just for an interesting discussion. You can email me at 
-                  julianatangerino2017@gmail.com! I will try to answer 
-                  as soon as I can. 🦄✨
-                  </p>
-
-                  <ContentTangs>
-                     <Tag value={tags} />
-                  </ContentTangs>
-               </ContentTextAbout>
-            </ContainerAbout>
-
-            <ContainerPortfolio>
-               <Title text="Portfolio" />
-            </ContainerPortfolio>
-
          </Wrapper>
       </>
    );
